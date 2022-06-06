9fa0e0f362953a1b861a5389b22bb1ea2555d29d
jack9782 <j1a2c3k4111@gmail.com>
2022-06-07 01:37:35 +0800

    Add Vue version ETA

---
+++ ETA/js/324-legacy.52fdb429.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_test2"]=self["webpackChunkvue_test2"]||[]).push([[324],{2324:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("h1",[e._v("This is LightRail page.")])])}],i={name:"LightRailPage"},r=i,s=n(1001),l=(0,s.Z)(r,u,a,!1,null,"0a44153a",null),c=l.exports}}]);
+//# sourceMappingURL=324-legacy.52fdb429.js.map
