e0276b06c53f29f1d1e638d24328e356421825a3
jack9782 <105511236+jack9782@users.noreply.github.com>
2023-01-13 00:29:42 +0800

    Add recipe display project

---
+++ recipe-display/js/about.2bb9caa3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkrecipe_display"]=self["webpackChunkrecipe_display"]||[]).push([[443],{5399:function(e,n,s){s.r(n),s.d(n,{default:function(){return r}});var t=function(){var e=this;e._self._c;return e._m(0)},u=[function(){var e=this,n=e._self._c;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],i=s(1001),a={},l=(0,i.Z)(a,t,u,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about.2bb9caa3.js.map
